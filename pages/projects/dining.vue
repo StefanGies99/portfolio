f7d5d397e4e41def4cbf02d2a50af3e896d4f369
Stefan Gies <s.gies21@gmail.com>
2020-01-20 16:44:11 +0100

    carousel: Import VueCarousel globally preventing asset errors
    Signed-off-by: Stefan Gies <s.gies21@gmail.com>

--- pages/projects/dining.vue
+++ pages/projects/dining.vue
@@ -48,13 +48,9 @@
 </template>
 
 <script>
-import { Carousel, Slide } from 'vue-carousel'
 
 export default {
-  components: {
-    Carousel,
-    Slide
-  }
+
 }
 </script>
 
--- pages/projects/openremote.vue
+++ pages/projects/openremote.vue
@@ -48,13 +48,7 @@
 </template>
 
 <script>
-import { Carousel, Slide } from 'vue-carousel'
-
 export default {
-  components: {
-    Carousel,
-    Slide
-  }
 }
 </script>
 
